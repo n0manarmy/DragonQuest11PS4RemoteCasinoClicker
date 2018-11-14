0384b223c90ea393eaae90648141f6a5564ec3a8
Penn Wilbert <pwilbert@gmail.com>
2018-11-14 12:59:42 -0500

    updated window activity to look for remote play window being active

--- src/main.py
+++ src/main.py
@@ -6,19 +6,23 @@ import win32gui
 
 def main():
     windowName = "PS4 Remote Play" #find Remote Play Window
-    while(windowActive(windowName)): #While remote play window active, sleep 1sec then press enter
-        time.sleep(random.uniform(.5, 1)) #sleep between .5sec and 1sec
-        keyStroke('ENTER') #call keyStroke function with key ENTER
+    while(True): #While remote play window active, sleep 1sec then press enter
+        if(windowActive(windowName)):
+            time.sleep(random.uniform(.5, 1)) #sleep between .5sec and 1sec
+            keyStroke('ENTER') #call keyStroke function with key ENTER
+        else:
+            print(windowName + " is not active. waiting till window active. Ctrl +C to stop running")
+            time.sleep(3) #sleep for 3 seconds
     exit()
 
 def windowActive(windowName):
     windowID = win32gui.FindWindow(None, windowName) #get windowID for windowName
-    if windowID == 0: #if window not found, return false which exits
+    activeWindow = win32gui.GetForegroundWindow()
+    if windowID != activeWindow: #if window not found, return false which exits
         print("Window Not Active!")
         return False
     else:
         print("Window Active!") #window found, while loop in main will proceed.
-        win32gui.SetForegroundWindow(windowID) #moves window to forground
         return True
 
 def keyStroke(KEY):
